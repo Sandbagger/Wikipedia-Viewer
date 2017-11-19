6554291bc509469a568f3d8737355812684d6eb6
Sandbagger <wneal_19@yahoo.co.uk>
2017-11-19 11:50:38 +0000

    Complete revamp, utilizing fetch() API and improved UX

--- wikiViewer.js
+++ wikiViewer.js
@@ -1,130 +1,56 @@
-//Wait for DOM to load before adding event listener on button
 
-document.addEventListener('DOMContentLoaded', addListenerToButton);
+//Called oninput attribute of form
+function requestJSONP(url) {
+	var results = document.getElementById("results");
+	while(results.hasChildNodes()){
+		results.removeChild(results.lastChild);
+	}
 
-//callback for DOM load which adds a click event on the button
-function addListenerToButton() {
+	var searchTerm = document.getElementById("search").value;
 
-var button = document.getElementsByTagName("button")[0];
+	fetch("https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=" + searchTerm + "&limit=10", {
+    method: 'GET',
+    mode: 'cors',
+    headers: new Headers({
+      'Content-Type': 'text/plain'
+    })
+}).then(function(resp) {
+    //Convert to JSON
+    return resp.json();
+}).then(function(json){
+	var arrTitles = json[1];
+	var arrBlurb = json[2];
+	var arrUrl = json[3];
+	arrTitles.forEach(function(x, index, url){
+		createNewDiv(arrTitles[index], arrBlurb[index], url[index]);
+		})
+	})
 
-button.addEventListener("click", saveText);	
-console.log("addListenerToButton");
-};
+}
 
-//callback for click event that triggers request to API
-function saveText(event) {
-	event.preventDefault()
-	var userInput = document.getElementsByTagName("input")[0].value;
+
+var createNewDiv = function(title, blurb, url){
+	var a = document.createElement("a");
+	a.setAttribute("href", "https://en.wikipedia.org/wiki/" + url);
+	a.setAttribute("target", "_blank");
 	
-	if (userInput === "") {
-		window.open('https://en.wikipedia.org/wiki/Special:Random');
-		return 
-	}
 
+	var div = document.createElement("div");
+	div.setAttribute("class", "result");
+	div.classList.add('show');
 	
-	requestJSONP(userInput);
-	toggledisplayResults();
-	console.log("saveText");
+	var header = document.createElement("header");
+	header.textContent = title;
+	div.appendChild(header);
 
-};
+	var p = document.createElement('p');
+	p.textContent = blurb;
+	div.appendChild(p);
 
-//advoids cross browser origin error
-function requestJSONP(searchTerm) {
-	//dynamically create a script tag
-	var scriptTag = document.createElement("script");
-	//set url with user defined search parameter
-	scriptTag.src = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&limit=10&namespace=0&format=json&callback=module.store";
-	//append tag to head element 
-	document.getElementsByTagName("head")[0].appendChild(scriptTag);
-	console.log("got to JSONP");
-};
+	a.appendChild(div);
 
 
-//callback invoked when API sends reponse
-var module = (function(){
-	var storedValue = [];
-console.log("got to module");
-	return {
-		store: function(val){
-			storedValue = [];
-			storedValue.push(val);
-			console.log("got to module.store");
-			module.updateDisplay();
-		},
-
-		retrieve: function(){
-			return storedValue;
-		},
-		searchTerm: function(){
-			return storedValue[0][0];
-		},
-
-		searchResultTitles: function(){
-			return storedValue[0][1];
-		},
-
-		searchResultdescription: function(){
-			return storedValue[0][2];
-		},
-
-		searchResultUrl: function(){
-			return storedValue[0][3];
-		},
-
-		updateDisplay: function(){
-			/*splayArray();*/
-			displaySearchTerm();
-			displayTitles();
-			displaySummary();
-			updateResultsUrl()
-		}
-	}
-
-}());
-
-function displayArray(){
-	var stored = module.retrieve();	
-	pTag = document.getElementsByTagName("p")[0];
-	text = pTag.textContent = stored;
+	document.getElementById('results').appendChild(a);
+	a.className = "fade-in";
 }
 
-function displaySearchTerm(){
-	var selectHeader = document.getElementsByTagName("p")[0];
-	selectHeader.textContent = "You searched for... " + module.searchTerm(); 
-}
-
-
-function displayTitles(){
-    var displayResults =  Array.prototype.slice.call(document.querySelectorAll(".result header"));
-    var titles = module.searchResultTitles();
-    for (var i = 0; i < displayResults.length; i++) {
-        var result = displayResults[i];
-        result.innerHTML = titles[i];
-        console.log(displayResults[i]);
-    }
-}
-
-function displaySummary(){
-	    var displayResults =  Array.prototype.slice.call(document.querySelectorAll(".result summary"));
-    var titles = module.searchResultdescription();
-    for (var i = 0; i < displayResults.length; i++) {
-        var result = displayResults[i];
-        result.innerHTML = titles[i];
-        console.log(displayResults[i]);
-	}
-}
-
-function updateResultsUrl(){
-	var displayResults =  Array.prototype.slice.call(document.querySelectorAll(".resultUrl"));
-	var links = module.searchResultUrl();
-	for (var i = 0; i < displayResults.length; i++) {
-        var result = displayResults[i];
-        result.setAttribute("href", links[i]);
-        console.log(displayResults[i]);
-	}
-}
-
-function toggledisplayResults(){
-    	var div = document.getElementsByTagName("main")[0];
-    div.style.display = "block";
-}
